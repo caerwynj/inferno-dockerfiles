69170a627020188a15d636af8b4bdb256a6eb34a
caerwyn <caerwynj@gmail.com>
2022-12-08 18:38:56 -0500

    arm64

--- create_manifest.sh
+++ create_manifest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker manifest create -a caerwyn/inferno-os:latest caerwyn/inferno-os-arm-linux:alpine-slim caerwyn/inferno-os-i386-linux:alpine-slim
+docker manifest create -a caerwyn/inferno-os:latest caerwyn/inferno-os-arm-linux:alpine-slim caerwyn/inferno-os-i386-linux:alpine-slim caerwyn/inferno-os-amd64-linux:latest caerwyn/inferno-os-arm64-linux:latest
 docker manifest push caerwyn/inferno-os:latest
 
